30edbb548fb1a13bb0dc5307c63d8dd0863d4094
lgc2333 <lgc2333@126.com>
2023-01-22 01:22:58 +0800

    99.9% complete(

---
+++ nonebot_plugin_picmcstat/__init__.py
@@ -0,0 +1,12 @@
+from nonebot.plugin import PluginMetadata
+
+from .__main__ import *
+
+
+__plugin_meta__ = PluginMetadata(
+    name="PicMCStat",
+    description="将一个 Minecraft 服务器的 MOTD 信息绘制为一张图片",
+    usage="使用 !motd 指令查看使用帮助",
+)
+
+__version__ = "0.1.0"
---
+++ nonebot_plugin_picmcstat/__main__.py
@@ -0,0 +1,26 @@
+from nonebot import on_command, require
+from nonebot.internal.adapter import Message
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg
+
+require("nonebot_plugin_imageutils")
+
+from .draw import ServerType, draw
+
+motd_handler = on_command("!motd", aliases={"！motd"})
+
+
+@motd_handler.handle()
+async def _(matcher: Matcher, arg: Message = CommandArg()):
+    arg = arg.extract_plain_text()
+
+    svr_type: ServerType = "je"
+    be_svr_prefix = ["pe", "be"]
+    for p in be_svr_prefix:
+        if arg.startswith(p):
+            arg = arg.replace(p, "", 1)
+            svr_type = "be"
+            break
+
+    arg = arg.strip()
+    await matcher.finish(await draw(arg, svr_type))
