f25bf56cc6233fb8d836c4431757c57f0a298362
lgc2333 <lgc2333@126.com>
2023-01-27 23:26:35 +0800

    adjust

--- nonebot_plugin_picmcstat/__init__.py
+++ nonebot_plugin_picmcstat/__init__.py
@@ -5,7 +5,7 @@ from .__main__ import *
 __plugin_meta__ = PluginMetadata(
     name="PicMCStat",
     description="将一个 Minecraft 服务器的 MOTD 信息绘制为一张图片",
-    usage="使用 !motd 指令查看使用帮助",
+    usage="使用 motd 指令查看使用帮助",
 )
 
 __version__ = "0.2.0"
