090f3597732b7929bf4d48aaa44027c638b10df3
student_2333 <lgc2333@126.com>
2023-06-05 01:05:46 +0800

    🎉 NoneBot 2.0 🚀

--- nonebot_plugin_picmcstat/__init__.py
+++ nonebot_plugin_picmcstat/__init__.py
@@ -1,11 +1,16 @@
 from nonebot.plugin import PluginMetadata
 
 from . import __main__ as __main__
+from .config import ConfigClass
 
+__version__ = "0.3.2"
 __plugin_meta__ = PluginMetadata(
     name="PicMCStat",
     description="将一个 Minecraft 服务器的 MOTD 信息绘制为一张图片",
     usage="使用 motd 指令查看使用帮助",
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-picmcstat",
+    type="application",
+    config=ConfigClass,
+    supported_adapters=["~onebot.v11"],
+    extra={"License": "MIT", "Author": "student_2333"},
 )
-
-__version__ = "0.3.1"
